81b28dae6bbbb71d7213b81099d9a378f0a307c8
Anish <anish.s.shekhawat@gmail.com>
2016-11-28 04:33:45 -0800

    Fixed indentation in consumer.java

--- src/main/java/edu/sjsu/cs185/Consumer.java
+++ src/main/java/edu/sjsu/cs185/Consumer.java
@@ -18,47 +18,48 @@ public class Consumer {
 			System.err.println("usage: Consumer <topic> <threshold> <file>");
 			System.exit(1);
 		}
-        	// parse the command-line
-        	String topic = args[0];
+        
+		// parse the command-line
+        String topic = args[0];
 		Double threshold = Double.parseDouble(args[1]);
 		String file = args[2];
 
 		// setup consumer 
-        	KafkaConsumer<String, String> consumer;
-        	try (InputStream props = Resources.getResource("consumer.props").openStream()) {
-				Properties properties = new Properties();
-				properties.load(props);
-				if (properties.getProperty("group.id") == null) {
-					properties.setProperty("group.id", "group-" + new Random().nextInt(100000));
+		KafkaConsumer<String, String> consumer;
+		try (InputStream props = Resources.getResource("consumer.props").openStream()) {
+			Properties properties = new Properties();
+			properties.load(props);
+			if (properties.getProperty("group.id") == null) {
+				properties.setProperty("group.id", "group-" + new Random().nextInt(100000));
+			}
+			consumer = new KafkaConsumer<>(properties);
+			consumer.subscribe(Arrays.asList(topic));
+			int timeouts = 0;
+			
+			while (true) {
+				ConsumerRecords<String, String> records = consumer.poll(200);
+				if (records.count() == 0) {
+					timeouts++;
+				} 
+				else {
+					System.out.printf("Got %d records after %d timeouts\n", records.count(), timeouts);
+					timeouts = 0;
 				}
-				consumer = new KafkaConsumer<>(properties);
-        		consumer.subscribe(Arrays.asList(topic));
-        		int timeouts = 0;
-        		
-				while (true) {
-					ConsumerRecords<String, String> records = consumer.poll(200);
-					if (records.count() == 0) {
-						timeouts++;
+			
+				for (ConsumerRecord<String, String> record : records) {
+					// pull out the vibration delta from record
+					String[] tokens = record.value().split(",");
+					
+					// determine if it's greater than the threshold
+					if (Double.parseDouble(tokens[2]) > threshold)
+					{
+						// if it is greater, than write that record to the file
+						Files.append(record.value() + "\n", output, Charsets.UTF_8);
 					} 
-					else {
-						System.out.printf("Got %d records after %d timeouts\n", records.count(), timeouts);
-						timeouts = 0;
-					}
-				
-					for (ConsumerRecord<String, String> record : records) {
-						// pull out the vibration delta from record
-						String[] tokens = record.value().split(",");
-                        
-						// determine if it's greater than the threshold
-                        if (Double.parseDouble(tokens[2]) > threshold)
-						{
-							// if it is greater, than write that record to the file
-							Files.append(record.value() + "\n", output, Charsets.UTF_8);
-						} 
-					}
-        		}
-				
-    		}
+				}
+			}
+			
+		}
 
 	}
 }
